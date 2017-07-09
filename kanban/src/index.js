0ea88d2d65b5e725923376f5c87f509514ea3377
leo <fffweb@hotmail.com>
2017-07-10 06:30:28 +0800

    my kanban ch2

---
+++ kanban/src/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+// import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import KanbanBoard from './KanbanBoard';
+
+let cardsList = [
+  {
+    id: 1,
+    title: "Read the Book",
+    description: "I should read the whole book",
+    status: "in-progress",
+    tasks: []
+  },
+  {
+    id: 2,
+    title: "Write some code",
+    description:  "Code along with the samples in the book. The complete source can be found at [github](https://github.com/pro-react)", 
+    status: "todo",
+    tasks: [
+      {
+        id: 1,
+        name: "ContactList Example",
+        done: true
+      },
+      {
+        id: 2,
+        name: "Kanban Example",
+        done: false
+      },
+      {
+        id: 3,
+        name: "My own experiments",
+        done: false
+      }
+    ]
+  }
+];
+
+ReactDOM.render(<KanbanBoard cards={cardsList}/>, document.getElementById('root'));
+registerServiceWorker();
